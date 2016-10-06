6e1e82538b76e39123783941000c7b75d4723027
Stanislaw Halik <sthalik@misaki.pl>
2016-10-06 10:19:07 +0200

    stdafx: decruft

--- directshow/capture/amcap/stdafx.h
+++ directshow/capture/amcap/stdafx.h
@@ -2,22 +2,21 @@
 //      or project specific include files that are used frequently,
 //      but are changed infrequently
 
-#if !defined(AFX_STDAFX_H__08E7CDC6_7F58_11D2_8CF1_00A0C9441E20__INCLUDED_)
-#define AFX_STDAFX_H__08E7CDC6_7F58_11D2_8CF1_00A0C9441E20__INCLUDED_
-
 #if _MSC_VER > 1000
-#pragma once
+#   pragma once
 #endif // _MSC_VER > 1000
+
+#ifndef _CRT_SECURE_NO_WARNINGS
+#   define _CRT_SECURE_NO_WARNINGS
+#endif
 
 #define _WIN32_WINNT 0x0501
 
-#ifdef WIN32_LEAN_AND_MEAN
-#undef WIN32_LEAN_AND_MEAN
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 #endif
 
 #define _ATL_APARTMENT_THREADED
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-
 
 #include <windows.h>
 #include <streams.h>
@@ -25,9 +24,3 @@
 
 #include "DShowUtil.h"
 #include "smartptr.h"   // smart pointer class
-
-
-//{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
-#endif // !defined(AFX_STDAFX_H__08E7CDC6_7F58_11D2_8CF1_00A0C9441E20__INCLUDED)
